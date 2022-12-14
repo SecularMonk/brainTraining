01391248bd4cf711c72b7925623e55b1466a0644
Calum Craig <ccraig@itzamedia.com>
2022-12-14 21:51:34 +0000

    Testing using next-auth session

--- brain_training/pages/input.jsx
+++ brain_training/pages/input.jsx
@@ -3,78 +3,28 @@ import Footer from "../components/footer";
 import { useEffect, useState } from "react";
 import { Question, Answer } from "../classes";
 import styles from "../styles/Home.module.css";
+import Quiz from "../components/quiz";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-   const [answer, setAnswer] = useState(null);
-   const [correctAnswer, setCorrectAnswer] = useState(null);
-   const [options, setOptions] = useState([]);
-   const [question, setQuestion] = useState(null);
-   const [optionState, setOptionState] = useState(false);
-
-   useEffect(() => {
-      const newQuestion = new Question({ numItems: 3, textLength: 3 });
-      setQuestion(newQuestion);
-      const options = newQuestion.getOptions();
-      setOptions(options);
-      // const tempStatement = neestion(tempQuestion2);
-   }, [answer]);
-
-   useEffect(() => {
-      evaluateAnswer();
-   }, [optionState]);
-
-   return (
-      <div className={styles.container}>
-         <Head>
-            <title>SMART Brain Training</title>
-            <meta name="description" content="SMART Brain Training" />
-            <link rel="icon" href="/favicon.ico" />
-         </Head>
-
-         <main className={styles.main}>
-            {question && (
-               <div>
-                  <p>{question.getProblemStatement()}</p>
-                  <p>{question.getQuestion()}</p>
-               </div>
-            )}
-
-            {!!options &&
-               options.length !== 0 &&
-               options.map((Element) => {
-                  return (
-                     <button
-                        key={Element}
-                        onClick={() => {
-                           recordAnswer(Element);
-                        }}
-                     >
-                        {Element}
-                     </button>
-                  );
-               })}
-         </main>
-
-         <Footer />
-      </div>
-   );
-
-   function recordAnswer(answer) {
-      try {
-         console.log(`recording answer ${answer}`);
-         setAnswer(answer);
-         setOptionState(!optionState);
-      } catch (error) {
-         console.log(error);
-      }
-   }
-
-   function evaluateAnswer() {
-      try {
-         console.log(JSON.stringify({ correctAnswer, answer, result: correctAnswer === answer }));
-         return correctAnswer === answer;
-      } catch (error) {
-         console.log(error);
-      }
+   const { data: session } = useSession();
+   if (session) {
+      return (
+         <div>
+            <Head></Head>
+            <h1>Session</h1>
+            <button onClick={() => signOut()}>Sign out</button>
+            <Quiz></Quiz>
+         </div>
+      );
+   } else {
+      return (
+         <div>
+            <Head></Head>
+            <h1>No Session</h1>
+            <button onClick={() => signIn()}>Sign in</button>
+            <Quiz></Quiz>
+         </div>
+      );
    }
 }
