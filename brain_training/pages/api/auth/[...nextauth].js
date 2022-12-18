450239344c651d4dc71e7f0e3a8607e3795ad194
Calum Craig <ccraig@itzamedia.com>
2022-12-18 19:12:13 +0000

    Add JWT configuration, include userId in session.

--- brain_training/pages/api/auth/[...nextauth].js
+++ brain_training/pages/api/auth/[...nextauth].js
@@ -10,4 +10,46 @@ export default NextAuth({
          from: process.env.EMAIL_FROM,
       }),
    ],
+
+   database: process.env.MONGODB_URI,
+
+   jwt: {
+      secret: "n7AfjHavFckHLwPOzruDAsyR3mEhpewgQQf7E3ud1bzUy273RM",
+      // encryption: true,
+   },
+
+   session: {
+      jwt: true,
+      maxAge: 30 * 24 * 60 * 60, // DAYS * HOURS * MINS * SECS, 30 days
+      strategy: "jwt",
+   },
+
+   callbacks: {
+      signIn: async ({ user, account, profile, email, credentials }) => {
+         // console.log(JSON.stringify({ user, account, profile, email, credentials }));
+         return true;
+      },
+      jwt: async ({ token, user }) => {
+         // if (user) {
+         //    console.log(JSON.stringify({ user }));
+         //    token.userId = user.id;
+         // }
+         // if (account) {
+         //    console.log(JSON.stringify({ account }));
+         // }
+         // if (profile) {
+         //    console.log(JSON.stringify({ profile }));
+         // }
+         return Promise.resolve(token);
+      },
+
+      session: async ({ session, token, user }) => {
+         // console.log(JSON.stringify({ session, token, user }));
+         if (token) {
+            session.user.userId = token.userId;
+         }
+         // session.user.userId = user.userId;
+         return Promise.resolve(session);
+      },
+   },
 });
