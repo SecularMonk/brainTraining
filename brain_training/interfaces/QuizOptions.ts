6f5db0e01c9adf502ffa99035fd4ac2c8e5d2731
Calum Craig <ccraig@itzamedia.com>
2022-12-31 09:12:23 +0000

    Add interface for quiz options

---
+++ brain_training/interfaces/QuizOptions.ts
@@ -0,0 +1,10 @@
+export interface QuizOptionsParameters {
+   selected: string;
+   setSelected: any;
+   setStarted: any;
+   numQuestions: number;
+}
+
+export interface DifficultyOptions {
+   text: string;
+}
