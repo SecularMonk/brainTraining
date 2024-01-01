a291f01f206b744478826893b91b95c668e2757c
Calum Craig <ccraig@itzamedia.com>
2024-01-01 15:06:41 +0000

    Add Typescript & config

---
+++ realmApp/src/generateXP.ts
@@ -0,0 +1,66 @@
+// TRIGGER: Quizzes.complete === true
+exports = async function (arg) {
+   const SERVICE_NAME = "Main";
+   const DB_NAME = "live";
+   const CONFIG_DB_NAME = "configuration";
+   const REWARD_TYPE = "XP";
+
+   // Get a collection from the context
+   const liveCollections = {
+      transactions: context.services.get(SERVICE_NAME).db(DB_NAME).collection("transactions"),
+      quizzes: context.services.get(SERVICE_NAME).db(DB_NAME).collection("quizzes"),
+   };
+
+   const configCollections = {
+      XP: context.services.get(SERVICE_NAME).db(CONFIG_DB_NAME).collection(REWARD_TYPE),
+   };
+
+   const { userId, _id: quizId, difficulty, score, complete, rewards } = arg?.fullDocument ?? {};
+
+   if (rewards && rewards?.length > 0 && rewards.some((element) => element.rewardType === REWARD_TYPE)) {
+      console.log(`${REWARD_TYPE} already earned for user ${userId} and quiz ${quizId}.`);
+      return;
+   }
+
+   const accrualConfiguration: any = await configCollections.XP.findOne({ configType: "accrual" });
+   const generationConfiguration: any = await configCollections.XP.findOne({ configType: "generation" });
+   console.log("accrualConfiguration: ", JSON.stringify(accrualConfiguration));
+   console.log("generationConfiguration: ", JSON.stringify(generationConfiguration));
+
+   const { XPAccrualConstant } = accrualConfiguration ?? {};
+   const { levelModifiers } = generationConfiguration ?? {};
+
+   const appliedLevelModifier = levelModifiers?.[difficulty] ?? 1;
+   const totalXPEarned = (XPAccrualConstant ? XPAccrualConstant : 1) * score * appliedLevelModifier;
+
+   const transaction = {
+      userId,
+      event: "rewardEarned",
+      rewardType: REWARD_TYPE,
+      amount: totalXPEarned,
+      eventDetails: {
+         quizId,
+      },
+   };
+
+   console.log("transaction: ", JSON.stringify(transaction));
+
+   const transactionInsertResult = await liveCollections.transactions.insertOne(transaction);
+   const { insertedId } = transactionInsertResult;
+
+   console.log("transactionInsertResult: ", JSON.stringify(transactionInsertResult));
+
+   const quizUpdateResult = liveCollections.quizzes.updateOne(
+      { _id: quizId },
+      {
+         $push: {
+            rewards: {
+               transactionId: insertedId,
+               rewardType: REWARD_TYPE,
+               rewardAmount: totalXPEarned,
+            },
+         },
+      }
+   );
+   console.log("quizUpdateResult: ", JSON.stringify(quizUpdateResult));
+};
---
+++ realmApp/src/realm.d.ts
@@ -0,0 +1,67 @@
+export {};
+
+export type Services = {
+   (name: string): import("mongodb").MongoClient;
+   // add your other services here
+   // (name: string): any;
+};
+
+declare global {
+   type ValueOf<T> = T[keyof T];
+   namespace context {
+      const services: {
+         get: Services;
+      };
+      const functions: {
+         execute: (name: string, ...args: any[]) => any;
+      };
+      const environment: {
+         tag: string;
+      };
+      const values: {
+         get: (value: any) => any;
+      };
+      const user: {
+         id: string;
+         type: "normal" | "server" | "system";
+         data: object;
+         custom_data: object;
+         identities: any[];
+      };
+      const http: {
+         get: () => any;
+         post: (options: PostOptions) => PostResponse;
+      };
+   }
+
+   type PostOptions = {
+      url: string;
+      body: {
+         [key: string]: any;
+      };
+      headers?: {
+         Authorization?: string[];
+         "Content-Type"?: ["application/json"];
+         Accept?: ["application/json"];
+      };
+      encodeBodyAsJSON?: boolean;
+   };
+
+   type PostResponse = {
+      status: string;
+      statusCode: HTTPStatusCodes;
+      body: PostResponseBody;
+   };
+
+   type PostResponseBody = {
+      text: () => string;
+   };
+
+   type HTTPStatusCodes = 200 | 400 | 404 | 500;
+   namespace BSON {
+      const ObjectId: (value?: any) => void;
+   }
+   namespace EJSON {
+      const parse: (value?: any) => any;
+   }
+}
